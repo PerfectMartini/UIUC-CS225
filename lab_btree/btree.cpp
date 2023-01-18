8be7ed1f32cbe88f69be4eea3e2bc03370930d16
PerfectMartini <geonkang119@gmail.com>
2023-01-18 18:55:35 +0900

    create BTree::find

--- lab_btree/btree.cpp
+++ lab_btree/btree.cpp
@@ -31,21 +31,54 @@ template <class K, class V>
 V BTree<K, V>::find(const BTreeNode* subroot, const K& key) const
 {
     /* TODO Your code goes here! */
+
     /* If first_larger_idx is a valid index and the key there is the key we
      * are looking for, we are done. */
+    if (subroot->elements[first_larger_idx].key == key)
+    {
+        return subroot->elements[first_larger_idx].value;
+    }
 
     /* Otherwise, we need to figure out which child to explore. For this we
-     * can actually just use first_larder_idx directly. E.g.
-     * | 1 | 5 | 7 | 8 |
-     * Suppose we are looking for 6. first_larger_idx is 2. This means we want to
-     * explore the child between 5 and 7. The children vector has a pointer for
+     * can actually just use first_larger_idx directly. E.g.
+     * | 0 | 5 | 7 | 8 |
+     * Suppose we are looking for 5. first_larger_idx is 2. This means we want to
+     * explore the child between 4 and 7. The children vector has a pointer for
      * each of the horizontal bars. The index of the horizontal bar we want is
-     * 2, which is conveniently the same as first_larger_idx. If the subroot is
+     * 1, which is conveniently the same as first_larger_idx. If the subroot is
      * a leaf and we didn't find the key in it, then we have failed to find it
      * anywhere in the tree and return the default V.
      */
-    return V();
+    if (subroot->is_leaf)
+    {
+        return V();
+    }
+    
+    return find(subroot->children[first_larger_idx], key);
+
 }
+template <class K, class V>
+V BTree<K,V>::find(const BTreeNode* subroot, const K& key) const
+{
+    size_t first_larger_idx = 0;
+    while(first_larger_idx < subroot->elements.size() && subroot->elements[first_larger_idx].key < key)
+    {
+        first_larger_idx++;
+    }
+    if(first_larger_idx < subroot->elements.size() && subroot->elements[first_larger_idx].key == key)
+    {
+        return subroot->elements[first_larger_idx].value;
+    }
+    else if(subroot->is_leaf)
+    {
+        return V();
+    }
+    else
+    {
+        return find(subroot->children[first_larger_idx], key);
+    }
+}
+
 
 /**
  * Inserts a key and value into the BTree. If the key is already in the
